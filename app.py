e5eef0500faa3ad001f9095f8c4288bf83446698
Sajid576 <sajidahmed696@gmail.com>
2021-12-11 22:09:54 +0600

    Add first layer logic for detecting common malicious codes

--- app.py
+++ app.py
@@ -1,17 +1,51 @@
 
 from flask import *
-from NoSql.feature_extractor import NoSqlFeatureExtractor
-from NoSql.nosql_injection_classifier import NoSqlInjection
-from Sql.feature_extractor import  SqlFeatureExtractor
-from Sql.sql_injection_classifier import  SqlInjection
+from second_layer.NoSql.feature_extractor import NoSqlFeatureExtractor
+from second_layer.NoSql.nosql_injection_classifier import NoSqlInjection
+from second_layer.Sql.feature_extractor import SqlFeatureExtractor
+from second_layer.Sql.sql_injection_classifier import SqlInjection
+from first_layer.common_malicious_payload_detector import *
 
 app = Flask(__name__)
 
 @app.route("/",methods=['GET'])
 def  mainRoute():
-  return "<h1>Welcome to the SQL/NoSQL Injection Detection Machine Learning Model API</h1>"
-  
-# Route for detecting SQL Injection
+  return "<h1>Welcome to the SQL and NoSQL Injection Detection Microservice</h1>"
+
+
+@app.route("/malicious_payloads", methods=['POST'])
+def detect_malicious_payloads_for_sql():
+    try:
+      payload_type = request.get_json()['payload_type']
+      if(payload_type == 'sql'):
+        print("SQL")
+        second_layer_check = detect_malicious_sql(request.get_json()['data'])
+        print(second_layer_check)
+        res = {
+            'message': "Successful",
+            'error': 'NULL',
+            'body': {
+                'result': second_layer_check
+            }
+        }
+        return res
+      else:
+        print("NoSQL")
+        second_layer_check = detect_malicious_nosql(request.get_json()['data'])
+        res = {
+            'message': "Successful",
+            'error': 'NULL',
+            'body': {
+                'result': second_layer_check
+            }
+        }
+        return res
+
+    except Exception as e:
+        print(e)
+        res = {"message": "Internal server error occurred"}
+        return res
+# Route for detecting SQL Injection by 2nd level
 @app.route("/sql_injection",methods=['POST'])
 def detectSqlInjection():
     try:
@@ -26,20 +60,13 @@ def detectSqlInjection():
                 }
         return res
     except Exception as e:
-                  print("Oops!", e.message, "occurred.")
-                  res={
-                        'message':e.message,
-                        'error':"Err",
-                        'body':{
-                          'result':""
-                        }
-                      }
-                  return res
+        print(e)
+        res = {"message": "Internal server error occurred"}
+        return res
 
 
-
-# Route for detecting NoSQL Injection
-@app.route("/nosql_injection",methods=['POST'])
+# Route for detecting NoSQL Injection by 2nd-level
+@app.route("/nosql_injection", methods=['POST'])
 def detectNoSqlInjection():
     try:
           feature_extractor_obj= NoSqlFeatureExtractor(request.get_json()['query'] )
@@ -55,14 +82,8 @@ def detectNoSqlInjection():
             }
           return res
     except Exception as e:
-        print("Oops!", e, "occurred.")
-        res={
-              'message':e,
-              'error':"Err",
-              'body':{
-                'result':""
-              }
-            }
+        print(e)
+        res = {"message": "Internal server error occurred"}
         return res
   
 
